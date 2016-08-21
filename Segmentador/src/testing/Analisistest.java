9913cd3b0af2ef672e261deb49158140547b7a91
jcarranzafdz <play3mordortec@gmail.com>
2016-08-20 20:29:02 -0600

    creaicon de Junit Test para HSV

--- Segmentador/src/testing/Analisistest.java
+++ Segmentador/src/testing/Analisistest.java
@@ -1,14 +1,17 @@
 package testing;
 
 import static org.junit.Assert.*;
+
 import main.Preprocesador.*;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import main.MotorAlgoritmia.HSV;
 
 public class Analisistest {
-
+    public HSV convertidorHSV;
+    
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
